e0e5a2b9f28eb6a1e57922d90584ef4b711ba8de
Moody <asdas201888@gmail.com>
2022-11-15 12:07:08 +0200

    provide testing to the RemindersListViewModel and its live data objects

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,104 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.pauseDispatcher
+import kotlinx.coroutines.test.resumeDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
+@Config(maxSdk = Build.VERSION_CODES.P)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var fakeRepo: FakeDataSource
+    private lateinit var remindersListViewModel: RemindersListViewModel
+
+    @Before
+    fun createRepository() {
+        stopKoin()
+
+        fakeRepo = FakeDataSource()
+        remindersListViewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(),
+            fakeRepo
+        )
+    }
+
+    @Test
+    fun loadRemindersWhenRemindersAreUnavailable() = runBlockingTest {
+
+        fakeRepo.setShouldReturnError(true)
+        remindersListViewModel.loadReminders()
+        MatcherAssert.assertThat(
+            remindersListViewModel.showSnackBar.getOrAwaitValue(),
+            CoreMatchers.`is`("Reminders not found")
+        )
+
+    }
+
+    @Test
+    fun noData() = runBlockingTest {
+        fakeRepo.deleteAllReminders()
+        remindersListViewModel.loadReminders()
+
+        MatcherAssert.assertThat(
+            remindersListViewModel.showNoData.getOrAwaitValue(),
+            CoreMatchers.`is`(true)
+        )
+    }
+    private fun getReminder(): ReminderDTO {
+        return ReminderDTO(
+            title = "title",
+            description = "desc",
+            location = "loc",
+            latitude = 47.5456551,
+            longitude = 122.0101731)
+    }
+    @Test
+    fun showLoading_withdata() = runBlocking {
+        fakeRepo.deleteAllReminders()
+        val reminder = getReminder()
+        fakeRepo.saveReminder(reminder)
+
+        mainCoroutineRule.pauseDispatcher()
+        remindersListViewModel.loadReminders()
+
+        MatcherAssert.assertThat(
+            remindersListViewModel.showLoading.getOrAwaitValue(),
+            CoreMatchers.`is`(true)
+        )
+        mainCoroutineRule.resumeDispatcher()
+
+        MatcherAssert.assertThat(
+            remindersListViewModel.showLoading.getOrAwaitValue(),
+            CoreMatchers.`is`(false)
+        )
+        MatcherAssert.assertThat(
+            remindersListViewModel.showNoData.getOrAwaitValue(),
+            CoreMatchers.`is`(false)
+        )
+    }
 }
