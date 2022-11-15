642512f585538bd9b094922f902d7f450344be96
Moody <asdas201888@gmail.com>
2022-11-15 11:59:45 +0200

    add Impl to FakeDataSource.kt

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,25 +4,47 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders: MutableList<ReminderDTO> = mutableListOf()) :
+    ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private var shouldReturnError = false
 
-    override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+    fun setShouldReturnError(shouldReturn: Boolean) {
+        this.shouldReturnError = shouldReturn
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
-    }
-
-    override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        reminders.add(reminder)
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders.clear()
+    }
+
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+
+        return if (shouldReturnError) {
+            Result.Error("Reminders not found", 404)
+        } else {
+            Result.Success(ArrayList(reminders))
+        }
+
     }
 
 
+    override suspend fun getReminder(id: String): Result<ReminderDTO> {
+
+        return if (shouldReturnError) {
+            Result.Error("Error")
+        } else {
+
+            val reminder = reminders.find { it.id == id }
+
+            if (reminder != null) {
+                Result.Success(reminder)
+            } else {
+                Result.Error("Reminder not found", 404)
+            }
+        }
+    }
 }
